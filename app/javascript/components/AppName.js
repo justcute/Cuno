ea6f28be1925c03bb705f57996a26ce9a4d95295
justcute <justcuteuwu@gmail.com>
2021-08-03 18:01:34 +0300

    add react

---
+++ app/javascript/components/AppName.js
@@ -0,0 +1,17 @@
+import React from "react"
+import PropTypes from "prop-types"
+class AppName extends React.Component {
+  render () {
+    return (
+      <React.Fragment>
+        <h1>{this.props.name} - {this.props.direction} portal</h1>
+      </React.Fragment>
+    );
+  }
+}
+
+AppName.propTypes = {
+  name: PropTypes.string,
+  direction: PropTypes.string
+};
+export default AppName
